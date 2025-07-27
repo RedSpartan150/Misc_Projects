8ea61659fec351ccdb138181cf1f9da91bfc421b
Red Spartan <167484111+RedSpartan150@users.noreply.github.com>
2025-07-27 10:20:12 -0700

    Delete password.py

--- password.py
+++
@@ -1,9 +0,0 @@
-import random
-alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"
-print("please input into the console how long you want your random password to be")
-x = int(input())
-endpassword = ""
-for i in range(0, x):
-    num = random.randint(0, 72)
-    endpassword = endpassword + alph[num]
-print(endpassword)
